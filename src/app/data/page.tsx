fcf0077e0b85a31f8abccc115b80e5101c048288
Muhammad Fuad Fakhruzzaki <fuadzaki00@gmail.com>
2025-08-30 05:09:02 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <h1 class="text-3x

--- src/app/data/page.tsx
+++ src/app/data/page.tsx
@@ -10,7 +10,7 @@ export default async function DataPage() {
     <AppLayout>
       <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
         <div className="flex items-center justify-between space-y-2">
-          <h1 className="text-3xl font-bold tracking-tight">Data Responden</h1>
+          
         </div>
         <Card>
             <CardHeader>
