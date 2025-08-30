9db5d6c0397bb4e27638c642596b08fad4d11dff
Muhammad Fuad Fakhruzzaki <fuadzaki00@gmail.com>
2025-08-30 05:08:31 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <h1 class="text-3x

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <AppLayout>
       <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
         <div className="flex items-center justify-between space-y-2">
-          <h1 className="text-3xl font-bold tracking-tight">Identitas Responden</h1>
+          
         </div>
         <Card className="max-w-3xl mx-auto">
             <CardHeader>
